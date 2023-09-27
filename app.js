8be8a4b35337e7b3289876cda416407a00da1bf5
italomagnov <italomagnov@gmail.com>
2023-09-27 08:27:13 -0400

    correção de sintaxe

--- app.js
+++ app.js
@@ -12,5 +12,5 @@ app.put('/api/users/:id', userController.updateUser);
 app.delete('/api/users/:id', userController.deleteUser);
 
 app.listen({
-    port: process.env.PORT || 3333,
+    port: process.env.PORT ?? 3333,
 });
